f958a72febfd79300b0f57eec2096ed7decbd42e
Bart Jongejan <bart.jongejan@gmail.com>
2023-01-10 18:37:12 +0100

    Updated CATALINA_HOME

--- compileme.sh
+++ compileme.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 ant clean-all
 ant -Divy=true download-ivy 
-env CATALINA_HOME=/opt/tomcat/apache-tomcat-9.0.30/ ant -Divy=true -Dprod war
+env CATALINA_HOME=/opt/tomcat/apache-tomcat-9.0.37 ant -Divy=true -Dprod war
